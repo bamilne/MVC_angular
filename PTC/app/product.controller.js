a8150b07e257131a717cfda83613ef3858818056
unknown <bamilne24@gmail.com>
2016-07-22 15:57:56 -0700

    add files to gihub

---
+++ PTC/app/product.controller.js
@@ -0,0 +1,51 @@
+﻿(function () {
+  'use strict';
+
+  angular.module('app').controller('ProductController', ProductController);
+
+  function ProductController($http) {
+    var vm = this;
+    var dataService = $http;
+
+    vm.products = [];
+    vm.product = {
+      ProductId: 1,
+      ProductName: 'Video Training'
+    };
+    vm.searchCategories = [];
+    vm.searchInput = {
+        selectedCategory: {
+            CategoryId: 0,
+            CategoryName: ''
+        },
+        productName: ''
+    }
+
+    productList();
+    searchCategoriesList();
+
+    function productList() {
+      dataService.get("/api/Product")
+      .then(function (result) {
+        vm.products = result.data;
+      },
+      function (error) {
+        handleException(error);
+      });
+    }
+
+    function searchCategoriesList() {
+      dataService.get("/api/Category/GetSearchCategories")
+      .then(function (result) {
+        vm.searchCategories = result.data;
+      },
+      function (error) {
+        handleException(error);
+      });
+    }
+
+    function handleException(error) {
+      alert(error.data.ExceptionMessage);
+    }
+  }
+})();
